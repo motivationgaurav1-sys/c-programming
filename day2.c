1fea8f6b77b15abb306357e41ca0d8643b69465f
Gaurav Pundir <motivation.gaurav1@gmail.com>
2026-07-03 10:34:05 +0000

    output instructions

---
+++ day2.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main(){
+  printf("hello woorld\n");
+  printf("hello\nstudents\n");
+printf("hello \tworld\t\n");
+printf("hello\tstudents\n");
+printf("hello\bworld\b\n");
+printf("hello\bworld\n");
+printf("hello world\b");
+printf("hello world\r\n");
+printf("hello\rworld\r\n");
+printf("hello\rstudents\n");
+printf("hello students\r\n");
+printf("hello\\world\\\n");
+printf("hello world\\\n");
+printf("hello\"student\"\n");
+printf("hello stupid\"\n");
+printf("hello\'idiot\'\n");
+printf("hello idiot\'\n");
+int a=5;
+printf("%d\n",a);
+printf("value of a is %d\n",a);
+printf("%d%d",a);
+printf("%d\n%d\n",a);
+printf("%d%d\n",a);
+printf("%d%d%d\n",a*a,a*a*a);
+char m=6;
+printf("%d\n",m);
+printf("%d%d\n",m);
+printf("%d\n",m);
+float k=9;
+printf("%f\n",k);
+printf("%f\n",k*k);
+double v=9;
+printf("%f\n",v);
+printf("%f",v*v);
+
+}
